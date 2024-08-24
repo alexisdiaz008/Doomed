f8cdae162a6f58190e753cdfa2356a2f356e624c
ad <lexmasta@gmail.com>
2024-08-24 13:59:22 -0400

    setup Vite & Express

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
 
-export function App() {
+export default function App() {
+  return(
     <div>
-    <h1>Welcome to My React App!</h1>
-  </div>
+      <h1>Welcome to My React Appasdas!</h1>
+    </div>
+  )
 }
-
-export default App;
